a7a342bcec3c15b79834a5171791b122ce63dac4
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-22 11:14:08 -0500

    more fields examples and explanations

--- client/views/qfdetails/qfdetails.js
+++ client/views/qfdetails/qfdetails.js
@@ -1,4 +1,4 @@
-AutoForm.addHooks(["fields1","fields2","fields3","fields4","fields5","fields6","fields7"], {
+AutoForm.addHooks(["fields1","fields2","fields3","fields4","fields5","fields6","fields7","fields8"], {
   onError: function () {
     console.log("onError hook called with arguments", arguments);
     console.log("onError hook context:", this);
