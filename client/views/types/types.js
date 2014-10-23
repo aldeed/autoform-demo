294a4dbdc742e7e319247cebdee7ed0f195095fb
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-23 14:43:58 -0500

    input types page

--- client/views/types/types.js
+++ client/views/types/types.js
@@ -15,6 +15,14 @@ Template.types.helpers({
       }
     });
   },
+  typesSchema2: function () {
+    var sst = selectedSchemaType.get();
+    return new SimpleSchema({
+      typeTest: {
+        type: sst
+      }
+    });
+  },
   selectedType: function () {
     return selectedType.get();
   },
@@ -26,7 +34,44 @@ Template.types.helpers({
 Template.types.events({
   'change .type-selection': function (event) {
     selectedType.set(event.target.value);
-    selectedSchemaType.set(String);
-    selectedSchemaTypeAsString.set("String");
+  },
+  'change .schema-type-selection': function (event) {
+    var t = event.target.value;
+    selectedSchemaTypeAsString.set(t);
+
+    switch (t) {
+      case "String":
+        selectedSchemaType.set(String);
+        break;
+      case "Number":
+        selectedSchemaType.set(Number);
+        break;
+      case "Boolean":
+        selectedSchemaType.set(Boolean);
+        break;
+      case "Date":
+        selectedSchemaType.set(Date);
+        break;
+      case "[String]":
+        selectedSchemaType.set([String]);
+        break;
+      case "[Number]":
+        selectedSchemaType.set([Number]);
+        break;
+      case "[Boolean]":
+        selectedSchemaType.set([Boolean]);
+        break;
+      case "[Date]":
+        selectedSchemaType.set([Date]);
+        break;
+    }
   }
-})
+});
+
+AutoForm.addHooks(["types1", "types2", "types3"], {
+  onSubmit: function (doc) {
+    console.log("Submitted value:", doc.typeTest);
+    this.done();
+    return false;
+  }
+});
