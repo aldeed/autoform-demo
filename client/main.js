63c3e881d732a8e5af8bd1760d3467a77189a072
Eric Dobbertin <eric@dairystatedesigns.com>
2017-03-26 10:19:38 -0500

    Updates for add-on pkgs

--- client/main.js
+++ client/main.js
@@ -1,3 +1,6 @@
 import { $ } from 'meteor/jquery';
+import 'select2';
+import 'select2/dist/css/select2.css';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
 
 window.jQuery = window.$ = $;
